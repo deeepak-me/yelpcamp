9edd9d4d5a3d84dd948777a683b3e7cd3601bc82
deeepak-me <deepak.krishnan.e@gmail.com>
2024-02-08 20:38:12 +0530

    feat : A basic seed setup to get some dataset

---
+++ seeds/index.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { descriptors, places } = require("./seedHelpers");
+const Campground = require("../models/campground");
+
+//connecting to mongoose
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error :"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)]; //function to select random element from an array
+
+const seedDB = async () => {
+  await Campground.deleteMany({}); // to delete existing data
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close(); //close the connection
+});
+
+//it has nothing to do with the main app. it's just if we ever wanted  to seed our database
+//we now cam call this index.js, run it as node; it delete everything in our database, but it
+//replaces everything with some new campgrounds
