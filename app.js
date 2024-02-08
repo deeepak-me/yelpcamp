e5ae354f6dcfeee4b50496102502483a37d9c2c5
deeepak-me <deepak.krishnan.e@gmail.com>
2024-02-08 20:26:09 +0530

    feat: Basic express setup created

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+
+const Campground = require("./models/campground");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error :"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({
+    title: "My Backyard",
+    description: "cheap camp ground",
+  });
+  await camp.save();
+  res.send(camp);
+});
+
+app.listen(3000, () => {
+  console.log("Serving on port 3000");
+});
