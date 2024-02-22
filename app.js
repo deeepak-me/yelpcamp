0420bd1ed8f2cdb7d27549619f65c4cd4fc73997
deeepak-me <deepak.krishnan.e@gmail.com>
2024-02-23 02:22:54 +0530

    feat: Express Error class added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 
 const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
 
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
@@ -47,6 +48,8 @@ app.get("/campgrounds/new", (req, res) => {
 app.post(
   "/campgrounds",
   catchAsync(async (req, res, next) => {
+    if (!req.body.campground)
+      throw new ExpressError("Invalid Campground Data", 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`campgrounds/${campground._id}`);
@@ -95,8 +98,14 @@ app.delete(
   })
 );
 
+app.all("*", (req, res, next) => {
+  next(new ExpressError("page not found", 404));
+});
+
 app.use((err, req, res, next) => {
-  res.send("Something Went wrong !!!");
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Something Went wrong !!!";
+  res.status(statusCode).render("error", { err });
 });
 
 app.listen(3000, () => {
