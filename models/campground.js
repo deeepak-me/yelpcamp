077bebebd5f55f4b9bf380502eb3007886871204
deeepak-me <deepak.krishnan.e@gmail.com>
2024-02-27 23:49:39 +0530

    feat: Added reviews of an array of objectId

--- models/campground.js
+++ models/campground.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
 
-const Scheme = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const CampgroundSchema = new Scheme({
+const CampgroundSchema = new Schema({
   title: String,
   image: String,
   price: Number,
   description: String,
   location: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
