f9817e8ada41538b76421ef6184f3ac87de76d49
deeepak-me <deepak.krishnan.e@gmail.com>
2024-02-29 00:46:41 +0530

    feat: Campground delete middleware added

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review");
 
 const Schema = mongoose.Schema;
 
@@ -14,6 +15,17 @@ const CampgroundSchema = new Schema({
       ref: "Review",
     },
   ],
+});
+
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  console.log(doc);
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
