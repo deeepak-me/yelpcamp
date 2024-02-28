d3a1a74e3338c7542fe2221e5a714ff8fc9d70cd
deeepak-me <deepak.krishnan.e@gmail.com>
2024-02-28 17:03:48 +0530

    feat reviews populated

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.get(
   "/campgrounds/:id",
   catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findById(id);
+    const campground = await Campground.findById(id).populate("reviews");
     res.render("campgrounds/show", { campground });
   })
 );
