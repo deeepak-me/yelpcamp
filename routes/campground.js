003319246e44a552c8bb20182ee0b61950e65ea6
deeepak-me <deepak.krishnan.e@gmail.com>
2024-03-01 17:29:12 +0530

    feat: Breaking out campground routes

---
+++ routes/campground.js
@@ -0,0 +1,93 @@
+const express = require("express");
+
+const router = express.Router();
+
+const { campgroundSchema } = require("../schemas");
+const catchAsync = require("../utils/catchAsync");
+const ExpressError = require("../utils/ExpressError");
+const Campground = require("../models/campground");
+
+//Express validation middleware
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+// campgrounds index
+router.get(
+  "/",
+  catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+  })
+);
+
+//add new campground form
+router.get("/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+//creating the end points,submitting
+
+router.post(
+  "/",
+  validateCampground,
+  catchAsync(async (req, res, next) => {
+    // if (!req.body.campground)
+    //   throw new ExpressError("Invalid Campground Data", 400);
+
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`campgrounds/${campground._id}`);
+  })
+);
+
+// show a single campground
+router.get(
+  "/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id).populate("reviews");
+    res.render("campgrounds/show", { campground });
+  })
+);
+
+// route to get edit form
+router.get(
+  "/:id/edit",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render("campgrounds/edit", { campground });
+  })
+);
+
+router.put(
+  "/:id",
+  validateCampground,
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {
+      ...req.body.campground,
+    });
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+// Delete the campground
+
+router.delete(
+  "/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+  })
+);
+
+module.exports = router;
